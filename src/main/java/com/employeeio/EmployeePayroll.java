c9a7fab0a2b909c28940ba31a78bfc7af12ad50f
ASHUTOSH <ashutosh@192.168.1.2>
2020-10-16 15:26:53 +0530

    [Ashutosh]Added:Welcome Message

---
+++ src/main/java/com/employeeio/EmployeePayroll.java
@@ -0,0 +1,14 @@
+package com.employeeio;
+
+import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.*;
+
+public class EmployeePayroll {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Payroll Service");
+	}
+
+}
